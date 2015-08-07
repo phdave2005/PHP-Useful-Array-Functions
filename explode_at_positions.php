b0f5e605a45eb79f6c17f1799b80f88fe0750ad2
David Partyka <phdave2005@gmail.com>
2015-08-06 20:45:09 -0400

    Create explode_at_positions.php

---
+++ explode_at_positions.php
@@ -0,0 +1,43 @@
+<?php
+
+function explode_at_positions($str, $positionArray) {
+	
+	$positionCount = count($positionArray);
+	
+	if (is_array($str)) {
+	    $output = "The first argument must be a string";
+	} else if ($positionCount == 0) {
+	   	$output = "The position array must not be empty";
+	} else {
+		  $str = (string)$str; // treat numbers as strings
+		  $strLength = strlen($str);
+		  $is_substring = false;
+		
+		  if ($positionCount > $strLength) {
+			    $output = "The array contains more positions than there are characters in the string";
+		  } else {
+			    $reassembledStr = '';
+			    $explodeNeedle = "0";
+			    $counter = 1;
+			
+			    while(strpos($str, $explodeNeedle) !== false) {
+				
+				      $explodeNeedle .= $counter;
+				      $counter++;
+				
+			    }
+			
+		      for($i = 0; $i < $strLength; $i++) {
+				
+				      $reassembledStr .= (in_array($i, $positionArray)) ? $explodeNeedle : $str[$i];
+					
+		      }
+			
+			    $output = explode($explodeNeedle, $reassembledStr);
+		  } 
+	}
+	
+	  return $output;
+}
+
+?>
