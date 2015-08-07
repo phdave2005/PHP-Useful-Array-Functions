c04f90487bee9df766c5fba3a6308f3849a6f8ec
David Partyka <phdave2005@gmail.com>
2015-08-06 22:43:16 -0400

    Update explode_at_positions.php

--- explode_at_positions.php
+++ explode_at_positions.php
@@ -2,42 +2,37 @@
 
 function explode_at_positions($str, $positionArray) {
 	
-	$positionCount = count($positionArray);
+    $positionCount = count($positionArray);
 	
-	if (is_array($str)) {
-	    $output = "The first argument must be a string";
-	} else if ($positionCount == 0) {
-	   	$output = "The position array must not be empty";
-	} else {
-		  $str = (string)$str; // treat numbers as strings
-		  $strLength = strlen($str);
-		  $is_substring = false;
+    if (is_array($str) || is_null($str)) {
+        $output = "The first argument must be a string";
+    } else if ($positionCount == 0) {
+        $output = "The position array must not be empty";
+    } else {
+        $str = (string)$str; // treat numbers as strings
+        $strLength = strlen($str);
 		
-		  if ($positionCount > $strLength) {
-			    $output = "The array contains more positions than there are characters in the string";
-		  } else {
-			    $reassembledStr = '';
-			    $explodeNeedle = "0";
-			    $counter = 1;
+        if ($positionCount > $strLength) {
+            $output = "The array contains more positions than there are characters in the string";
+        } else {
+            $reassembledStr = '';
+            $explodeNeedle = "0";
+            $counter = 1;
 			
-			    while(strpos($str, $explodeNeedle) !== false) {
+            while(strpos($str, $explodeNeedle) !== false) {
 				
-				      $explodeNeedle .= $counter;
-				      $counter++;
+                $explodeNeedle .= $counter;
+	        $counter++;
 				
-			    }
+	    }
 			
-		      for($i = 0; $i < $strLength; $i++) {
-				
-				      $reassembledStr .= (in_array($i, $positionArray)) ? $explodeNeedle : $str[$i];
-					
-		      }
+	    for($i = 0; $i < $strLength; $i++) $reassembledStr .= (in_array($i, $positionArray)) ? $explodeNeedle : $str[$i];
 			
-			    $output = explode($explodeNeedle, $reassembledStr);
-		  } 
-	}
-	
-	  return $output;
+	    $output = explode($explodeNeedle, $reassembledStr);
+        }
+    }
+
+    return $output;
 }
 
 ?>
