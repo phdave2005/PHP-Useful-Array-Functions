6cb2119bc9ddd0c5d6ece574bdef22f45ff867c6
David Partyka <phdave2005@gmail.com>
2015-08-23 14:43:02 -0400

    Update explode_at_first_char.php

--- explode_at_first_char.php
+++ explode_at_first_char.php
@@ -1,36 +1,32 @@
 <?php
 function explode_at_first_char($str, $needle) {
 	
+    $pos = strpos($str, $needle);
+	
     if (is_array($str)) {
         $output = "The first argument must be a string";
     } else if (is_null($str) || strlen($needle) == 0) {
         $output = "The needle must not be null nor empty";
-    } else if (strpos($str, $needle) === false) {
+    } else if ($pos === false) {
         $output = array($str);
     } else {
         $str = (string)$str;//treat numbers as strings
         $strLength = strlen($str);
 		
-        $pos = strpos($str, $needle);
-		
         $reassembledStr = '';
-	      $explodeNeedle = '0';
-	      $counter = 1;
+        $explodeNeedle = '0';
+	$counter = 1;
 			
-	      while(strpos($str, $explodeNeedle) !== false) {
+	while(strpos($str, $explodeNeedle) !== false) {
+	    $explodeNeedle .= $counter;
+	    $counter++;
+        }
 			
-	          $explodeNeedle .= $counter;
-	          $counter++;
-		
-	      }
+	for($i = 0; $i < $strLength; $i++) {
+	    $reassembledStr .= ($i == $pos) ? $explodeNeedle : $str[$i];
+	}
 			
-			  for($i = 0; $i < $strLength; $i++) {
-				
-	          $reassembledStr .= ($i == $pos) ? $explodeNeedle : $str[$i];
-					
-	      }
-			
-	      $output = explode($explodeNeedle, $reassembledStr);
+	$output = explode($explodeNeedle, $reassembledStr);
     	
     }
 	
