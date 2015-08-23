6e14faf816d3f5a803c0be61532ebb686814c576
David Partyka <phdave2005@gmail.com>
2015-08-23 14:24:41 -0400

    Update multi_delimiter_explode.php

--- multi_delimiter_explode.php
+++ multi_delimiter_explode.php
@@ -3,25 +3,25 @@
 function multi_delimiter_explode($str, $delimiterArray) {
 	
     $arrayCount = count($delimiterArray);
-	  $str = (string)$str; // treat numbers as strings
+    $str = (string)$str; // treat numbers as strings
     $strLength = strlen($str);
 	
     if (is_array($str) || is_null($str) || $strLength == 0) {
         $output = "The first argument must be a string of length greater than zero";
     } else if (!is_array($delimiterArray) || $arrayCount == 0) {
         $output = "The second argument must be an array with at least one member";
-	  } else {
+    } else {
 	  
-		    $explodeDelimiter = '0';
-		    $counter = 1;
-		    while(strpos($str, $explodeDelimiter) !== false) {
+        $explodeDelimiter = '0';
+        $counter = 1;
+        while(strpos($str, $explodeDelimiter) !== false) {
             $explodeDelimiter .= $counter;
-	          $counter++;
-	      }
+            $counter++;
+        }
 		
-		    $substituteStr = str_replace($delimiterArray, $explodeDelimiter, $str);
+        $substituteStr = str_replace($delimiterArray, $explodeDelimiter, $str);
 		
-	      $output = explode($explodeDelimiter, $substituteStr);;
+        $output = explode($explodeDelimiter, $substituteStr);;
 		
     }
     
