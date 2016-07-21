4060062741fb76d359aff853220a46fbf51ac36d
David Partyka <phdave2005@gmail.com>
2016-07-21 17:58:20 -0400

    Update base_n_sort.php

--- base_n_sort.php
+++ base_n_sort.php
@@ -1,84 +1,94 @@
 <?php
 
-  class base_n_sort {
+class base_n_sort {
 
-    function group($arr, $base, $direction) {
-	    $count = count($arr);
-	    $masterArray = $keysArray = array();
-	    $keyCount = 0;
-	    $inserted = false;
-	    for($i = 0; $i < $count; $i++) {
-		    $thisLength = strlen((string)$arr[$i]);
-		    if (!in_array((string)$thisLength, $keysArray)) {
-			    $tempArray = array();
-			    $minusOne = $keyCount - 1;
-			    $masterArray[(string)$thisLength] = array($arr[$i]);
-			    if ($keyCount === 0) {
-				    $keysArray[] = $thisLength;
-			    } else {
-				    if ($direction === 'asc') {
-					    for($j = 0; $j < $keyCount; $j++) {
-						    if (!$inserted) {
-							    if ($j !== $minusOne) {
-								    if ($thisLength <= (int)$keysArray[$j]) {
-									    $tempArray[] = $thisLength;
-									    $inserted = true;
-								    }
-								    $tempArray[] = $keysArray[$j];
-							    } else {
-								    if ($thisLength <= (int)$keysArray[$j]) {
-									    $tempArray[] = $thisLength;
-									    $tempArray[] = $keysArray[$j];
-								    } else {
-								    	$tempArray[] = $keysArray[$j];
-									    $tempArray[] = $thisLength;
-								    }
-							    }
-						    } else {
-							    $tempArray[] = $keysArray[$j];
-						    }
-					    }
-				    } else if ($direction === 'desc') {
-					    for($j = 0; $j < $keyCount; $j++) {
-						    if (!$inserted) {
-							    if ($j !== $minusOne) {
-								    if ($thisLength >= (int)$keysArray[$j]) {
-									    $tempArray[] = $thisLength;
-									    $inserted = true;
-								    }
-								    $tempArray[] = $keysArray[$j];
-							    } else {
-								    if ($thisLength >= (int)$keysArray[$j]) {
-									    $tempArray[] = $thisLength;
-									    $tempArray[] = $keysArray[$j];
-								    } else {
-									    $tempArray[] = $keysArray[$j];
-									    $tempArray[] = $thisLength;
-								    }
-							    }
-						    } else {
-							    $tempArray[] = $keysArray[$j];
-						    }
-					    }
-				    } else {
-					    throw new Exception('The order must be either ascending ("asc") or descending ("desc")');
-				    }
-				    $keysArray = $tempArray;
-				    $inserted = false;
-			    }
-			    $keyCount++;
-		    } else {
-			    $thisArray = $masterArray[(string)$thisLength];
-			    $thisArray[] = $arr[$i];
-			    $masterArray[(string)$thisLength] = $thisArray;
-		    }
-	    }
-	    //Now sort master array
-	    for($i = 0; $i < $keyCount; $i++) {
-		    $returnArray[(string)$keysArray[$i]] = $masterArray[(string)$keysArray[$i]];
-	    }
-	    return $this->sortMinis($returnArray, $base, $direction);;
+	public function __construct($sort_type) {
+		if ($sort_type !== 'asc' && $sort_type !== 'desc') $sort_type = 'asc';
+		$this->for_loop_group = "group_" . $sort_type;
+		$this->direction = $sort_type;
     }
+
+	function group_asc($tempArray, $keyCount, $minusOne, $thisLength, $keysArray) {
+		$inserted = false;
+		for($j = 0; $j < $keyCount; $j++) {
+			if (!$inserted) {
+				if ($j !== $minusOne) {
+					if ($thisLength <= (int)$keysArray[$j]) {
+						$tempArray[] = $thisLength;
+						$inserted = true;
+					}
+					$tempArray[] = $keysArray[$j];
+				} else {
+					if ($thisLength <= (int)$keysArray[$j]) {
+						$tempArray[] = $thisLength;
+						$tempArray[] = $keysArray[$j];
+					} else {
+						$tempArray[] = $keysArray[$j];
+						$tempArray[] = $thisLength;
+					}
+				}
+			} else {
+				$tempArray[] = $keysArray[$j];
+			}
+		}	
+		return $tempArray;
+	}
+	
+	function group_desc($tempArray, $keyCount, $minusOne, $thisLength, $keysArray) {
+		$inserted = false;
+		for($j = 0; $j < $keyCount; $j++) {
+			if (!$inserted) {
+				if ($j !== $minusOne) {
+					if ($thisLength >= (int)$keysArray[$j]) {
+						$tempArray[] = $thisLength;
+						$inserted = true;
+					}
+					$tempArray[] = $keysArray[$j];
+				} else {
+					if ($thisLength >= (int)$keysArray[$j]) {
+						$tempArray[] = $thisLength;
+						$tempArray[] = $keysArray[$j];
+					} else {
+						$tempArray[] = $keysArray[$j];
+						$tempArray[] = $thisLength;
+					}
+				}
+			} else {
+				$tempArray[] = $keysArray[$j];
+			}
+		}
+		return $tempArray;
+	}
+
+	function sort($arr, $base) {
+		$count = count($arr);
+		$masterArray = $keysArray = array();
+		$keyCount = 0;
+		for($i = 0; $i < $count; $i++) {
+			$thisLength = strlen((string)$arr[$i]);
+			if (!in_array((string)$thisLength, $keysArray)) {
+				$tempArray = array();
+				$minusOne = $keyCount - 1;
+				$masterArray[(string)$thisLength] = array($arr[$i]);
+				if ($keyCount === 0) {
+					$keysArray[] = $thisLength;
+				} else {
+					$keysArray = $this->{$this->for_loop_group}($tempArray, $keyCount, $minusOne, $thisLength, $keysArray);
+				}
+				$keyCount++;
+			} else {
+				$thisArray = $masterArray[(string)$thisLength];
+				$thisArray[] = $arr[$i];
+				$masterArray[(string)$thisLength] = $thisArray;
+			}
+		}
+		//Now sort master array
+		for($i = 0; $i < $keyCount; $i++) {
+			$returnArray[(string)$keysArray[$i]] = $masterArray[(string)$keysArray[$i]];
+		}
+		$returnArray = $this->sortMinis($returnArray, $base, $this->direction);
+		return $returnArray;
+	}
 
     function convertToDecimal($val, $base) {
 	    $valLength = strlen($val);
@@ -155,6 +165,6 @@
 	    }
 	    return $sortedArray;
     }
-  }
+}
   
 ?>
