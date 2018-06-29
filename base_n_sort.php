b2e981b3f197703b6ee054be5cb40ec94fdaadcc
David Partyka <phdave2005@gmail.com>
2018-06-29 15:33:43 -0400

    Corrected some errors in sorter

--- base_n_sort.php
+++ base_n_sort.php
@@ -3,173 +3,90 @@
 class base_n_sort {
 	public function __construct($sort_type) {
 		if ($sort_type !== 'asc' && $sort_type !== 'desc') $sort_type = 'asc';
-		$this->for_loop_group = "group_" . $sort_type;
-		$this->for_loop_sort = "sort_" . $sort_type;
+		$this->sort_function = ($sort_type === 'asc') ? 'asort' : 'rsort';
     }
-	private function group_asc($tempArray, $keyCount, $minusOne, $thisLength, $keysArray) {
-		$inserted = false;
-		for($j = 0; $j < $keyCount; $j++) {
-			if (!$inserted) {
-				if ($j !== $minusOne) {
-					if ($thisLength <= (int)$keysArray[$j]) {
-						$tempArray[] = $thisLength;
-						$inserted = true;
-					}
-					$tempArray[] = $keysArray[$j];
-				} else {
-					if ($thisLength <= (int)$keysArray[$j]) {
-						$tempArray[] = $thisLength;
-						$tempArray[] = $keysArray[$j];
-					} else {
-						$tempArray[] = $keysArray[$j];
-						$tempArray[] = $thisLength;
-					}
-				}
-			} else {
-				$tempArray[] = $keysArray[$j];
-			}
-		}	
-		return $tempArray;
-	}
 	
-	private function group_desc($tempArray, $keyCount, $minusOne, $thisLength, $keysArray) {
-		$inserted = false;
-		for($j = 0; $j < $keyCount; $j++) {
-			if (!$inserted) {
-				if ($j !== $minusOne) {
-					if ($thisLength >= (int)$keysArray[$j]) {
-						$tempArray[] = $thisLength;
-						$inserted = true;
-					}
-					$tempArray[] = $keysArray[$j];
-				} else {
-					if ($thisLength >= (int)$keysArray[$j]) {
-						$tempArray[] = $thisLength;
-						$tempArray[] = $keysArray[$j];
-					} else {
-						$tempArray[] = $keysArray[$j];
-						$tempArray[] = $thisLength;
-					}
+	private function is_valid_input($arr, $base) {
+		$is_valid = true;
+		
+		foreach($arr as $a) {
+			$split = str_split((string)$a);
+			foreach($split as $s) {
+				if ((int)$s >= $base) {
+					$is_valid = false;
+					break 2;
 				}
-			} else {
-				$tempArray[] = $keysArray[$j];
 			}
-		}
-		return $tempArray;
-	}
-	public function sort($arr, $base) {
-		$count = count($arr);
-		$masterArray = $keysArray = array();
-		$keyCount = 0;
-		for($i = 0; $i < $count; $i++) {
-			$thisLength = strlen((string)$arr[$i]);
-			if (!in_array((string)$thisLength, $keysArray)) {
-				$tempArray = array();
-				$minusOne = $keyCount - 1;
-				$masterArray[(string)$thisLength] = array($arr[$i]);
-				if ($keyCount === 0) {
-					$keysArray[] = $thisLength;
-				} else {
-					$keysArray = $this->{$this->for_loop_group}($tempArray, $keyCount, $minusOne, $thisLength, $keysArray);
-				}
-				$keyCount++;
-			} else {
-				$thisArray = $masterArray[(string)$thisLength];
-				$thisArray[] = $arr[$i];
-				$masterArray[(string)$thisLength] = $thisArray;
-			}
-		}
-		//Now sort master array
-		for($i = 0; $i < $keyCount; $i++) {
-			$returnArray[(string)$keysArray[$i]] = $masterArray[(string)$keysArray[$i]];
 		}
 		
-		return $this->sortMinis($returnArray, $base);
+		return $is_valid;
 	}
-    private function convertToDecimal($val, $base) {
-	    $valLength = strlen($val);
-	    $revVal = strrev((string)$val);
-	    $convertedVal = 0;
-	    for($i = 0; $i < $valLength; $i++) {
-		    $convertedVal += ((int)$revVal[$i] !== 0) ? pow(($base * (int)$revVal[$i]), $i) : 0;
-	    }
-	    return $convertedVal;	
-    }
-    
-    private function sort_asc($buildArray, $val, $base, $thisCount) {
-    	$inserted = false;
-    	for($j = 1; $j < $thisCount; $j++) {
-			$buildCount = count($buildArray);
-			$minusOne = $buildCount - 1;
-			$tempArray = array();
-			for($k = 0; $k < $buildCount; $k++) {
-				if (!$inserted) {
-					$convertedVal = $this->convertToDecimal($val[$j], $base);
-					$convertedBuild = $this->convertToDecimal($buildArray[$k], $base);
-					if ($k !== $minusOne) {
-						if ($convertedVal <= $convertedBuild) {
-							$tempArray[] = $val[$j];
-							$inserted = true;
-						}
-						$tempArray[] = $buildArray[$k];
-					} else {
-						if ($convertedVal <= $convertedBuild) {
-							$tempArray[] = $val[$j];
-							$tempArray[] = $buildArray[$k];
-						} else {
-					    	$tempArray[] = $buildArray[$k];
-							$tempArray[] = $val[$j];	
-						}
+	
+	public function sort($arr, $base) {
+		
+		if ($this->is_valid_input($arr, $base)) {
+		
+			$convert_base_n_to_decimal = function($val, $base) {
+				$val_length = strlen($val);
+				$rev_val = strrev((string)$val);
+				$converted_val = 0;
+				for($i = 0; $i < $val_length; $i++) {
+					if ((int)$rev_val[$i] > 0) {
+						$converted_val += pow($base, $i) * (int)$rev_val[$i];
 					}
-				} else {
-					$tempArray[] = $buildArray[$k];	
 				}
-		    }
-		    $buildArray = $tempArray;
-		}
-    	return $buildArray;
-    }
-    
-    private function sort_desc($buildArray, $val, $base, $thisCount) {
-    	$inserted = false;
-    	for($j = 1; $j < $thisCount; $j++) {
-			$buildCount = count($buildArray);
-			$minusOne = $buildCount - 1;
-			$tempArray = array();
-			for($k = 0; $k < $buildCount; $k++) {
-				if (!$inserted) {
-					$convertedVal = $this->convertToDecimal($val[$j], $base);
-					$convertedBuild = $this->convertToDecimal($buildArray[$k], $base);
-					if ($k !== $minusOne) {
-						if ($convertedVal >= $convertedBuild) {
-							$tempArray[] = $val[$j];
-							$inserted = true;
-						}
-						$tempArray[] = $buildArray[$k];
+				return $converted_val;	
+			};
+			
+			$convert_decimal_to_base_n = function($val, $base) use(&$convert_decimal_to_base_n) {
+				$valLength = strlen($val);
+				$remainder = $val;
+				$converted_val = '';
+				for($i = 0; $i < $valLength; $i++) {
+					if ($remainder < $base) {
+						$converted_val .= $remainder;
+						$remainder = null;
+						break;
 					} else {
-						if ($convertedVal >= $convertedBuild) {
-							$tempArray[] = $val[$j];
-							$tempArray[] = $buildArray[$k];
-						} else {
-					    	$tempArray[] = $buildArray[$k];
-							$tempArray[] = $val[$j];	
+						$floor = (int)floor($val / $base);
+						if ($floor >= $base) {
+							$floor = $convert_decimal_to_base_n($floor, $base);
 						}
+						$converted_val .= $floor;
+						$remainder = $remainder % $base;
 					}
-				} else {
-					$tempArray[] = $buildArray[$k];	
 				}
-		    }
-		    $buildArray = $tempArray;
+				
+				if (isset($remainder)) $converted_val .= $remainder;
+				
+				return (int)$converted_val;	
+			};
+			
+			$mapped = array_map(function ($el) use($convert_base_n_to_decimal, $base) {
+				return $convert_base_n_to_decimal($el, $base);	
+			}, $arr);
+			
+			$sort_function = $this->sort_function;
+			$sort_function($mapped);
+			$orig = $mapped;
+			
+			$mapped = array_map(function ($el) use($convert_decimal_to_base_n, $base) {
+				return $convert_decimal_to_base_n($el, $base);	
+			}, $mapped);
+			
+			print_r($orig);
+			echo "<br>";
+			print_r($mapped);
+			die();
+		} else {
+			echo "Cannot be mapped";
+			exit;
 		}
-    	return $buildArray;
-    }
-    private function sortMinis($masterArray, $base) {
-	    $sortedArray = array();
-	    foreach($masterArray as $key=>$val) {
-		    $sortedArray = array_merge($sortedArray, $this->{$this->for_loop_sort}(array($val[0]), $val, $base, count($val)));
-	    }
-	    return $sortedArray;
-    }
+				
+		return $mapped;
+	}
+    
 }
+
   
 ?>
