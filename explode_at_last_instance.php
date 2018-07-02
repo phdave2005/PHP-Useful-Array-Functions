42f51d5f91bd236f9864c840fb13371387209c7d
David Partyka <phdave2005@gmail.com>
2018-07-02 12:49:22 -0400

    Made more readable

--- explode_at_last_instance.php
+++ explode_at_last_instance.php
@@ -1,33 +1,25 @@
 <?php
-function explode_at_last_instance($str, $delimiter) {
-	
-    $pos = strrpos($str, $delimiter);
-	
-    if (is_array($str)) {
+
+function explode_at_last_instance($str = false, $delimiter = false) {
+		
+    if (is_array($str) || is_object($str) || is_null($str) || is_bool($str)) {
         throw new Exception("The first argument must be a string");
-    } else if (is_null($str) || strlen($delimiter) == 0) {
+    } else if (strlen($delimiter) === 0) {
         throw new Exception("The delimiter must not be null nor empty");
     } else {
+	$pos = strrpos($str, $delimiter);
     	if ($pos === false) {
-        	$output = array($str);
+	    $output = array($str);
     	} else {
         	$str = (string)$str;//treat numbers as strings
-        	$strLength = strlen($str);
-		
-        	$reassembledStr = '';
-		$explodeDelimiter = '0';
+        			
+        	$pre = substr($str, 0, $pos);
+		$post = substr($str, $pos + strlen($str));
 			
-		while(strpos($str, $explodeDelimiter) !== false) $explodeDelimiter .= $explodeDelimiter;
-			
-		for($i = 0; $i < $strLength; $i++) $reassembledStr .= ($i == $pos) ? $explodeDelimiter : $str[$i];
-			
-		$output = explode($explodeDelimiter, $reassembledStr);
-    	
-	 }
+		$output = [$pre, $post];    	
+    	}
 	
     	return $output;
-
-    	
     }
 }
 
