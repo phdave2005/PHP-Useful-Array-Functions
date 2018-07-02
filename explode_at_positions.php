20401d418d44e8112acd8df44c783bd3d30199d7
David Partyka <phdave2005@gmail.com>
2018-07-02 13:25:22 -0400

    Update explode_at_positions.php

--- explode_at_positions.php
+++ explode_at_positions.php
@@ -1,12 +1,12 @@
 <?php
 
-function explode_at_positions($str, $positionArray) {
+function explode_at_positions($str = false, $positionArray = array()) {
 	
-    $positionCount = count($positionArray);
+    $positionCount = count($positionArray);	
 	
-    if (is_array($str) || is_null($str)) {
+    if (is_array($str) || is_object($str) || is_null($str) || is_bool($str)) {
         throw new Exception("The first argument must be a string");
-    } else if ($positionCount == 0) {
+    } else if ($positionCount === 0) {
         throw new Exception("The position array must not be empty");
     } else {
         $str = (string)$str; // treat numbers as strings
@@ -15,20 +15,20 @@ function explode_at_positions($str, $positionArray) {
         if ($positionCount > $strLength) {
             throw new Exception("The array contains more positions than there are characters in the string");
         } else {
-            $reassembledStr = '';
-            $explodeNeedle = "0";
-            $counter = 1;
-			
-            while(strpos($str, $explodeNeedle) !== false) {
-				
-                $explodeNeedle .= $counter;
-	        $counter++;
-				
-	    }
-			
-	    for($i = 0; $i < $strLength; $i++) $reassembledStr .= (in_array($i, $positionArray)) ? $explodeNeedle : $str[$i];
-			
-	    return explode($explodeNeedle, $reassembledStr);
+            
+			$firstPiece = substr($str, 0, $positionArray[0]);	
+			$pieces = array($firstPiece);
+
+			$counter = 0;
+			for($i = $positionArray[0]; $i < $strLength; $i++) {
+				if (in_array($i, $positionArray)) {
+					$len = isset($positionArray[$counter + 1]) ? $positionArray[$counter + 1] - $positionArray[$counter] : $strLength - $positionArray[$counter];
+					$pieces[] = substr($str, $i, $len);
+					$counter++;
+				}
+			}
+
+			return $pieces;
         }
     }
 }
